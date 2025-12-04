f4190d52ab5ab5d43d3ad14866ea49e3784dbc1d
Daniel Tollenaar <daniel@d2hydro.nl>
2025-12-04 16:58:56 +0100

    setup test

---
+++ src/ribasim_tools/tests/test_drainage_from_modflow.py
@@ -0,0 +1,6 @@
+from ribasim_tools.get_drainage_from_modflow import budgets_from_dir
+from ribasim_tools import settings
+
+
+settings.source_data_dir / "GRAM3_2"/ "25" / "GRAM32_BASIS1_TA-PRJ" / "RESULTS"/ "BASIS1_TA-PRJ"
+budgets = budgets_from_dir(modflow_budgets_path=)
